3c02cd98a3b1944782aeeac9ff5e22c4614cb265
Ekaterina <litra1122@gmail.com>
2023-10-10 20:07:06 +0300

    Added addition function

--- matrix_calc.py
+++ matrix_calc.py
@@ -46,4 +46,14 @@ class MatrixCalc:
 
     @staticmethod
     def addition(matrix1, matrix2):
-        pass
+        if len(matrix1) != len(matrix2) or len(matrix1[0]) != len(matrix2[0]):
+            raise MatrixDoNotMatchException()
+
+        result = []
+        for i in range(len(matrix1)):
+            row = []
+            for j in range(len(matrix1[0])):
+                row.append(matrix1[i][j] + matrix2[i][j])
+            result.append(row)
+
+        return result
