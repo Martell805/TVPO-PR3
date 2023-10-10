a3f75ee0027358e14b0e15038a1692db16add563
Martell <vovasamb@yandex.ru>
2023-10-10 19:35:36 +0300

    Fixed MatrixCalc.determinant

--- matrix_calc.py
+++ matrix_calc.py
@@ -3,5 +3,26 @@ from matrix_exceptions import MatrixDoNotMatchException
 
 class MatrixCalc:
     @staticmethod
+    def __get_sub_matrix(i, j, matrix):
+        return [
+            [
+                matrix[q][w] for w in range(len(matrix)) if w != j
+            ] for q in range(len(matrix)) if q != i
+        ]
+
+    @staticmethod
     def determinant(matrix):
-        ...
+        rows, cols = len(matrix), len(matrix[0])
+        if cols != rows:
+            raise MatrixDoNotMatchException()
+
+        if rows == 2:
+            return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+        result = 0
+
+        for j, element in enumerate(matrix[0]):
+            multiplier = 1 if j % 2 == 0 else -1
+            result += multiplier * element * MatrixCalc.determinant(MatrixCalc.__get_sub_matrix(0, j, matrix))
+
+        return result
